3fb7fe779ee7cb2ee7becfa7289a9292a5e9fc17
Andrius Dobrotinas <andrius.dobrotinas@gmail.com>
2016-08-27 16:06:05 +0300

    Missed this one last time

---
+++ Factory/DTOs/TypeInstance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSeeker
+{
+    public struct TypeInstance
+    {
+        public Type Type { get; set; }
+        public object Instance { get; set; }
+    }
+}
