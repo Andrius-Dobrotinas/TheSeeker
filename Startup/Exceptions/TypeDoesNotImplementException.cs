3e2ccbb2b1eb5d835476914278df32a6f62cb373
Andrius Dobrotinas <andrius.dobrotinas@gmail.com>
2016-08-28 20:49:06 +0300

    Moved exception class to a separate file

---
+++ Startup/Exceptions/TypeDoesNotImplementException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheSeeker.Startup
+{
+    /// <summary>
+    /// An exception that indicates that the type does not implement the target type
+    /// </summary>
+    public class TypeDoesNotImplementException : Exception
+    {
+        public Type Type { get; }
+        public Type TargetType { get; }
+
+        public TypeDoesNotImplementException(Type type, Type targetType) :
+            base($"Type \"{type.AssemblyQualifiedName}\" does not implement \"{targetType.AssemblyQualifiedName}\"")
+        {
+            Type = type;
+            TargetType = targetType;
+        }
+    }
+}
